5548feb3fc90cbb1c6d63ff6a2a30ffed5b3137f
Cristian Minrod Molina López <urgenciologominrod@gmail.com>
2026-04-09 02:33:24 -0600

    fix: disable pwa to unblock vercel deploy

--- next.config.js
+++ next.config.js
@@ -3,17 +3,7 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: /^https?.*/,
-      handler: 'NetworkFirst',
-      options: {
-        cacheName: 'remeinia-cache',
-        expiration: { maxEntries: 200, maxAgeSeconds: 86400 },
-      },
-    },
-  ],
+  disable: true,
 });
 
 const nextConfig = {
@@ -22,7 +12,6 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-  // Preparado para internacionalización futura
   i18n: {
     locales: ['es'],
     defaultLocale: 'es',
